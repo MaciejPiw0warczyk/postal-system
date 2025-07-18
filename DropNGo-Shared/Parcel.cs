a84c733f172c972cc14206040b138ca92811b42e
Maciej Piwowarczyk <mpiwo4@gmail.com>
2025-07-18 14:10:51 +0200

    Added project for shared resources

---
+++ DropNGo-Shared/Parcel.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace DropNGo_Shared;
+
+public class Parcel
+{
+    public Parcel()
+    {
+    }
+
+    public Parcel(string tracking, Enums.ParcelStatus status)
+    {
+        TrackingId = tracking;
+        Status = status;
+    }
+    
+    public string TrackingId { get; set; }
+    public Enums.ParcelStatus Status { get; set; } //TODO make enum for statuses
+    
+}
